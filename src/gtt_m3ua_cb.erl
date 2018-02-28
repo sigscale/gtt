8fa37fb42a8fb8ffa3c8937a25b3e245a34053ba
Vance Shipley <vances@sigscale.org>
2018-02-28 16:16:05 +0530

    add m3ua callback module

---
+++ src/gtt_m3ua_cb.erl
@@ -0,0 +1,42 @@
+%%% gtt_m3ua_cb.erl
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% @copyright 2018 SigScale Global Inc.
+%%% @end
+%%% Licensed under the Apache License, Version 2.0 (the "License");
+%%% you may not use this file except in compliance with the License.
+%%% You may obtain a copy of the License at
+%%%
+%%%     http://www.apache.org/licenses/LICENSE-2.0
+%%%
+%%% Unless required by applicable law or agreed to in writing, software
+%%% distributed under the License is distributed on an "AS IS" BASIS,
+%%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%%% See the License for the specific language governing permissions and
+%%% limitations under the License.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% @doc This {@link //m3ua/m3ua_asp_fsm. m3ua_asp_fsm} or
+%%% 	{@link //m3ua/m3ua_sgp_fsm. m3ua_sgp_fsm} behaviour callback
+%%%	module implements a handler for a User Service Access Point (USAP)
+%%% 	of an M3UA stack SAP in the {@link //gtt. gtt} application.
+%%%
+-module(gtt).
+-copyright('Copyright (c) 2018 SigScale Global Inc.').
+
+%% m3ua_asp_fsm callbacks
+-export([]).
+
+-include("gtt.hrl").
+
+%%----------------------------------------------------------------------
+%%  The gtt_m3ua_cb public API
+%%----------------------------------------------------------------------
+
+%%----------------------------------------------------------------------
+%%  The m3ua_[asp|sgp]_fsm callabcks
+%%----------------------------------------------------------------------
+
+
+%%----------------------------------------------------------------------
+%%  internal functions
+%%----------------------------------------------------------------------
+
