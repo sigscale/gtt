ce00b168b16f240de183a239c6269d2f401db396
Vance Shipley <vances@sigscale.org>
2018-03-15 17:24:29 +0530

    start as before ep

--- src/gtt_app.erl
+++ src/gtt_app.erl
@@ -69,44 +69,23 @@ start1() ->
 	end.
 %% @hidden
 start2(TopSup) ->
-	F = fun() -> mnesia:all_keys(gtt_ep) end,
-	case mnesia:transaction(F) of
-		{atomic, EndPoints} ->
-			start3(TopSup, EndPoints);
-		{aborted, Reason} ->
-			{error, Reason}
-	end.
-%% @hidden
-start3(TopSup, [EP | T]) ->
-	case gtt:start_ep(EP) of
-		ok ->
-			start3(TopSup, T);
-		{error, Reason} ->
-			error_logger:error_report(["Failed to start SCTP endpoint",
-					{endpoint, EP}, {reason, Reason}, {module, ?MODULE}]),
-			start3(TopSup, T)
-	end;
-start3(TopSup, []) ->
-	start4(TopSup).
-%% @hidden
-start4(TopSup) ->
 	F = fun() -> mnesia:all_keys(gtt_as) end,
 	case mnesia:transaction(F) of
 		{atomic, ASs} ->
 			Children = supervisor:which_children(TopSup),
 			{_, AsFsmSup, _, _} = lists:keyfind(gtt_as_fsm_sup, 1, Children),
-			start5(TopSup, AsFsmSup, ASs);
+			start3(TopSup, AsFsmSup, ASs);
 		{aborted, Reason} ->
 			{error, Reason}
 	end.
 %% @hidden
-start5(TopSup, AsFsmSup, [AS | T]) ->
+start3(TopSup, AsFsmSup, [AS | T]) ->
 	F = fun() -> mnesia:read(gtt_as, AS, read) end,
-	start6(TopSup, AsFsmSup, mnesia:transaction(F), T);
-start5(TopSup, _, []) ->
-	{ok, TopSup}.
+	start4(TopSup, AsFsmSup, mnesia:transaction(F), T);
+start3(TopSup, _, []) ->
+	start5(TopSup).
 %% @hidden
-start6(TopSup, AsFsmSup, {atomic, [#gtt_as{name = Name, role = Role,
+start4(TopSup, AsFsmSup, {atomic, [#gtt_as{name = Name, role = Role,
 		na = NA, keys = Keys, mode = Mode, min_asp = Min, max_asp = Max}]}, T) ->
 	StartMod = gtt_as_fsm,
 	StartArgs = [Name, Role, NA, Keys, Mode, Min, Max],
@@ -114,14 +93,35 @@ start6(TopSup, AsFsmSup, {atomic, [#gtt_as{name = Name, role = Role,
 	case supervisor:start_child(AsFsmSup,
 			[{global, Name}, StartMod, StartArgs, StartOpts]) of
 		{ok, _Child} ->
-			start5(TopSup, AsFsmSup, T);
+			start3(TopSup, AsFsmSup, T);
 		{error, Reason} ->
 			error_logger:error_report(["Failed to start Application Server",
 					{as, Name}, {reason, Reason}, {module, ?MODULE}]),
-			start5(TopSup, AsFsmSup, T)
+			start3(TopSup, AsFsmSup, T)
 	end;
-start6(_, _, {aborted, Reason}, _) ->
+start4(_, _, {aborted, Reason}, _) ->
 	{error, Reason}.
+%% @hidden
+start5(TopSup) ->
+	F = fun() -> mnesia:all_keys(gtt_ep) end,
+	case mnesia:transaction(F) of
+		{atomic, EndPoints} ->
+			start6(TopSup, EndPoints);
+		{aborted, Reason} ->
+			{error, Reason}
+	end.
+%% @hidden
+start6(TopSup, [EP | T]) ->
+	case gtt:start_ep(EP) of
+		ok ->
+			start6(TopSup, T);
+		{error, Reason} ->
+			error_logger:error_report(["Failed to start SCTP endpoint",
+					{endpoint, EP}, {reason, Reason}, {module, ?MODULE}]),
+			start6(TopSup, T)
+	end;
+start6(TopSup, []) ->
+	{ok, TopSup}.
 
 -spec start_phase(Phase :: atom(), StartType :: start_type(),
 		PhaseArgs :: term()) -> ok | {error, Reason :: term()}.
